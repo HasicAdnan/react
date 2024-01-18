7d7aed2252eed2550884ac6d0a069f18c81f4c1b
HasicAdnan <adnan.hasic@hotmail.com>
2024-01-18 09:27:22 +0100

    add .idea folder to .gitignore

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <header className="App-header">
             <h1>This is my App</h1>
             <List key={"my-key"} title={"Hallo"} subtitle={"Du"}>
-            <h3>Ein Kind</h3>
+            <h3>Ein Kinddddd</h3>
             <h3>Noch Ein Kind</h3>
             </List>
         </header>
